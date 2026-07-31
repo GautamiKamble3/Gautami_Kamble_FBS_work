c816268cc83e3cb3f2c26a79a9c94e21d8960842
Gautami Kamble <gautamikamble153@gmail.com>
2026-07-31 12:13:11 +0530

    Assignment 7

---
+++ Core Python/Assignments/Assignment7/ass7_d.py
@@ -0,0 +1,21 @@
+# pattern d
+
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(1, n - i + 1):
+        print(" ", end=" ")
+
+    # Increasing numbers
+    num = i
+    for j in range(1, i + 1):
+        print(num, end=" ")
+        num += 1
+
+    # Decreasing numbers
+    num -= 2
+    for j in range(1, i):
+        print(num, end=" ")
+        num -= 1
+
+    print()
---
+++ Core Python/Assignments/Assignment7/ass7_e.py
@@ -0,0 +1,31 @@
+# pattern e
+
+n = 5
+
+for i in range(1, n + 1):
+    # Print leading spaces
+    for j in range(1, n - i + 1):
+        print(" ", end=" ")
+
+    if i == 1:
+        print(1)
+    elif i == n:
+        # Print last row
+        for j in range(1, n + 1):
+            if j == n:
+                print(j, end="")
+            else:
+                print(j, end=" ")
+            print(" ", end=" ")
+
+        print()
+
+    else:
+        # Print left side
+        print(1, end=" ")
+        # Print middle spaces
+        for j in range(1, 2 * i - 2):
+            print(" ", end=" ")
+
+        # Print right side
+        print(i)
