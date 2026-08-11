2a35e33c5b5d08ca9cad835dfdb07664515692d1
Gautami Kamble <gautamikamble153@gmail.com>
2026-08-11 14:04:39 +0530

    Update ass9_q10.py

--- Core Python/Assignments/Assignment9/ass9_q10.py
+++ Core Python/Assignments/Assignment9/ass9_q10.py
@@ -1,12 +1,10 @@
 # 10. Write a program to reverse a number using recursion.
 
-def calculateReverse(n):
-    rev = 0
-    while(n > 0):
-        dig = n % 10
-        n = n // 10
-        rev = rev * 10 + dig
-    return rev
+def calculateReverse(n, rev=0):
+    if n == 0:
+        return rev
+    dig = n % 10
+    return calculateReverse(n // 10, rev * 10 + dig)
 
 def reversedNum(n):
     res = calculateReverse(n)
