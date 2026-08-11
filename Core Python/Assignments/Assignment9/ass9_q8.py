cfe5e97bce22def69bab8f20d8551a18bf3baffc
Gautami Kamble <gautamikamble153@gmail.com>
2026-08-11 14:04:00 +0530

    Update ass9_q8.py

--- Core Python/Assignments/Assignment9/ass9_q8.py
+++ Core Python/Assignments/Assignment9/ass9_q8.py
@@ -1,16 +1,20 @@
 # 8. Write a program to check whether a number is prime or not using recursion.
 
+def isDivisible(n, i):
+    if i * i > n:
+        return False
+    if n % i == 0:
+        return True
+    return isDivisible(n, i + 1)
+
 def primeCal(n):
-    for num in range(n):
-        if (num > 1):
-            for i in range(2, num):
-                if num%i == 0:
-                    break
-            else:
-                return True
+    if n <= 1:
+        return False
+    return not isDivisible(n, 2)
+
 def checkPrime(n):
     res = primeCal(n)
-    if(res == True):
+    if res == True:
         print(f'{n} is prime number.')
     else:
         print(f'{n} is not a prime number.')
