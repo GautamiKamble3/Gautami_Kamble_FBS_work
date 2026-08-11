5ed7d2743ed9d9f4b6f0cc53fe26773d6b013e12
Gautami Kamble <gautamikamble153@gmail.com>
2026-08-11 13:58:57 +0530

    Update ass9_q1.py

--- Core Python/Assignments/Assignment9/ass9_q1.py
+++ Core Python/Assignments/Assignment9/ass9_q1.py
@@ -2,17 +2,15 @@
 # i. 1! + 2! + 3! + 4! +….. + n!
 #  Note : For fact and sum two recursive functions
 
-def factorial():
-    n = int(input('Enter nth number: '))
-    fact = 1
-    for i in range(1, n+1):
-        fact *= i
-    return fact
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
 
-def sumFactorial():
-    sum = 0
-    fact = factorial()
-    sum += fact 
-    print(f'Sum of factorial series is : {sum}')
-    
-sumFactorial()
+def sumFactorial(n):
+    if n == 1:
+        return factorial(1)
+    return factorial(n) + sumFactorial(n - 1)
+
+num = int(input('Enter nth number: '))
+print(f'Sum of factorial series is : {sumFactorial(num)}')
